0e2ee4b1c6511b56260146662848c645d0717f20
JoseGuiFerreira17 <joseguiferreira17@gmail.com>
2025-02-27 13:59:30 -0300

    feature add task

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { FormEvent, useState } from 'react';
 import styles from './App.module.css';
 import { Button } from './components/Button';
 import { Header } from './components/Header';
@@ -6,21 +7,46 @@ import { Empty } from './components/List/Empty';
 import { HeaderList } from './components/List/HeaderList';
 import { Item } from './components/List/Item';
 
+export interface ITask {
+  id: number;
+  text: string;
+  isChecked: boolean;
+}
+
 function App() {
+  const [tasks, setTasks] = useState<ITask[]>([]);
+  const [textTaskInput, setTextTaskInput] = useState('');
+
+  function handleAddTask(e: FormEvent) {
+    e.preventDefault();
+    if (!textTaskInput) return;
+
+    const newTask: ITask = {
+      id: Math.random(),
+      text: textTaskInput,
+      isChecked: false,
+    };
+    setTasks([...tasks, newTask]);
+    setTextTaskInput('');
+  }
   return (
     <main>
       <Header />
       <section className={styles.content}>
         <div className={styles.taskInfo}>
-          <Input />
-          <Button />
+          <Input
+            onChange={(e) => setTextTaskInput(e.target.value)}
+            value={textTaskInput}
+          />
+          <Button onClick={handleAddTask} />
         </div>
         <div className={styles.taskList}>
           <HeaderList />
           <div>
             <Item />
-            <Item />
-            <Item />
+            {tasks.map((task) => {
+              return <Item />;
+            })}
           </div>
           <Empty />
         </div>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,14 @@
 import { PlusCircle } from '@phosphor-icons/react';
 import styles from './Button.module.css';
 
-export function Button() {
+export function Button({
+  ...rest
+}: React.DetailedHTMLProps<
+  React.ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+>) {
   return (
-    <button className={styles.button}>
+    <button className={styles.button} {...rest}>
       Criar
       <PlusCircle size={16} />
     </button>
